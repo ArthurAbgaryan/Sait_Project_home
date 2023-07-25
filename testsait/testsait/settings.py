51b7d9482c75f497ae0cdc0eba34498249a8a0ee
Arthur <abgaryanarthurelina@gmail.com>
2023-07-25 23:14:53 +0300

    save all settings

--- testsait/testsait/settings.py
+++ testsait/testsait/settings.py
@@ -1,3 +1,13 @@
+#debug turbo
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+#end debug turbo
+
 """
 Django settings for testsait project.
 
@@ -53,33 +63,24 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
-
+    "debug_toolbar",
+    #'channels',
     'crispy_forms',
     'ckeditor',
+    'django_extensions',
     'django_cleanup.apps.CleanupConfig'
 ]
 
 SITE_ID = 1
 
 # Provider specific settings
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -90,7 +91,7 @@ ROOT_URLCONF = 'testsait.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -98,7 +99,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                
+                ...
             ],
         },
     },
@@ -114,8 +115,34 @@ AUTHENTICATION_BACKENDS = [
     
 ]
 
-WSGI_APPLICATION = 'testsait.wsgi.application'
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    },
+    'github': {
+        'SCOPE': [
+            'user',
+            'repo',
+            'read:org',
+        ],
+    }
+}
 
+WSGI_APPLICATION = 'testsait.wsgi.application'
+ASGI_APPLICATION = "testsait.routing.application" #1-ая перем название проекта,
+                                    #2-ая routing создается одноиме-ый файл routing.py
+
+CHANNEL_LAYERS = {
+    "default":{
+        "BACKEND":"channels.layers.InMemoryChannelLayer"
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -187,5 +214,35 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Установки crispy-forms
 CRISPY_TEMPLATE_PACK = 'uni_form'
-def save():
-    pass
+#django-ckeditor
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'width':'auto',
+    },
+}
+
+#End django-ckeditor
+
+GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv("GOOGLE_RECAPTCHA_SECRET_KEY")
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
+# End Email
+
+#теги определяющие цвета выводимых сообшений урок 25
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
+
+# в производстве убрать.
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
