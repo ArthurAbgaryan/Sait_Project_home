20fc629e792cb9f755b23687ef967267a4ca9ffc
Arthur <abgaryanarthurelina@gmail.com>
2023-07-26 22:28:58 +0300

    сохранение настроек settings,urls,routing

--- testsait/testsait/urls.py
+++ testsait/testsait/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('accounts/', include('allauth.urls')),
     #path("__debug__/", include("debug_toolbar.urls")),
+    #path('accounts/', include('allauth.urls')),
 ]
 
